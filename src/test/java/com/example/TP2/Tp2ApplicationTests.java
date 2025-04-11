df3f79b821fcbc3b31583e603753494f798c6c6d
Nehed <neheddridi888@gmail.com>
2025-04-11 10:26:31 +0100

    commit import

--- src/test/java/com/example/TP2/Tp2ApplicationTests.java
+++ src/test/java/com/example/TP2/Tp2ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.TP2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class Tp2ApplicationTests {
 
